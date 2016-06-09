c22879fb406659cbe02f028acc68c5e537177281
maheshhp <hpmahesh73@gmail.com>
2016-06-09 19:51:48 +0530

    find_valid_word
    Updated

--- AndroidCode.java
+++ AndroidCode.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package androidcode;
 
 import java.io.BufferedReader;
@@ -11,42 +6,34 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 /**
  *
  * @author mahesh
  */
 public class AndroidCode {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        
         File f = new File("/Users/mahesh/Documents/words.txt");
         BufferedReader br = new BufferedReader(new FileReader(f));
         String s;
-        boolean b;
+        int i=0;
         ArrayList ar = new ArrayList();
+        
         System.out.println("Enter length of string required");
         Scanner scan = new Scanner(System.in);
         int len = scan.nextInt();
+        
         while((s=br.readLine()) != null)
         {
             if(s.length() == len)
             {
-                HashSet hs = new HashSet();
-                b = false;
-                for(int i=0;i<len;i++)
-                {
-                    if(!(hs.add(s.charAt(i))))
-                    {
-                        b = true;
-                        break;
-                    }
-                }
-                if(!b)
+                TreeSet ts = new TreeSet();
+                for(i=0;i<len && ts.add(s.charAt(i));i++);
+                if(i == len)
                 {
                     System.out.println(s+" added");//for debug
                     ar.add(s);
